1ae291f2c9f0deffb2ec0305bfa8161852edca07
Teng <46767359+Teng97@users.noreply.github.com>
2020-04-06 21:29:56 +0200

    Main amb opcions per triar el mode de joc

--- Main.hs
+++ Main.hs
@@ -3,6 +3,7 @@ module Main where
 import System.IO
 import System.Directory
 import Data.Char
+import Data.List
 
 -- ens ha de crear un tauler i bloc a l'estat inicial.
 --sortida ::
@@ -19,9 +20,14 @@ main = do
  exists <- doesFileExist ("./" ++ nom ++ ".txt")
  if exists
  then do
-  fitxer <- openFile (nom ++ ".txt") ReadMode
-  contents <- hGetContents fitxer
-  putStrLn contents
-  hClose fitxer
+  contents <- readFile (nom ++ ".txt")
+  let list = lines contents
+  putStrLn "Vols que sigui interactiu el joc? S/N"
+  interactiu <- getLine
+  if interactiu == "S"
+  then do
+   print (list !! 6) -- mostra la llista 6
+  else
+   print (list !! 5) -- mostra la llista 5
  else
   putStrLn "El fitxer no existeix"
