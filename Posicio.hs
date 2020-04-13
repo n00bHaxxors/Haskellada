2be17989e98b002b47971f5136f033f992267040
Lluís Trilla <clank201@gmail.com>
2020-04-13 19:01:38 +0200

    Posicio done

--- Posicio.hs
+++ Posicio.hs
@@ -3,9 +3,14 @@ module Posicio (
  sumar
 ) where
 
+import Data.Tuple
+
 data Posicio = Posicio {
     x :: Int,
     y :: Int
 } deriving (Eq, Ord, Show, Read)
 
 sumar :: Posicio -> (Int, Int) -> Posicio
+sumar p s = result
+    where
+        result = Posicio {x = fst s + x p, y = snd s + y p}
