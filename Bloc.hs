19fb08c9d93547404ccafed6f09e9603d25ab31f
Lluís Trilla <clank201@gmail.com>
2020-04-13 18:31:05 +0200

    Bloc.hs definit

--- Bloc.hs
+++ Bloc.hs
@@ -1,27 +1,18 @@
 module Bloc (
- posBloc,
- --esLegal,
- --legals
+  Bloc,
+  aplicarMoviment,
+  PposicionsOcupades
 ) where
 
 import Posicio as Pos
 import Moviment
 
-type base = Int -- base 
-type altura = Int -- altura
-type gruix = Int -- gruix
+data Bloc = Bloc { x :: Int,
+    y :: Int,
+    z :: Int,
+    pos :: Pos
+    } deriving (Eq)
 
-data Estat = Dret | Tombat
+aplicarMoviment :: Bloc -> Moviment -> Bloc
 
--- ens ha de donar les posicions que ocupa el bloc.
-posBloc :: (Int, Int)
-posBloc = (Pos.posX , Pos.posY)
-
--- Ens ha de dir si tal com esta el bloc es possible o no fer cert moviment en cert
--- tauler, es a dir, si aquest moviment es legal o fa caure el bloc al buit.
---esLegal :: String -> Bool
---esLegal = True
-
--- ens ha de tornar els moviments que siguin legals en l'estat actual.
---legals :: [Moviment]
---legals =
+posicionsOcupades :: Bloc -> [Posicio]
